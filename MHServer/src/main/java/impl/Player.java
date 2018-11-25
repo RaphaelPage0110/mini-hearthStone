dccbf8e09a732885ba967e925678efc1298fe4a4
Raphael Page <page.raf@gmail.com>
2018-11-25 08:53:43 +0100

    ajout de la main du joueur dans la classe joueur

--- MHServer/src/main/java/impl/Player.java
+++ MHServer/src/main/java/impl/Player.java
@@ -7,80 +7,96 @@ import java.util.ArrayList;
 public class Player {
 
     private IdPlayer id;
+    private IdPlayer idOpponent;
     private Hero myHero;
     private <Minion> myMinions;
+    private <Card> myCards;
     private int myMana;
 
-	/**
-	* Returns value of id
-	* @return
-	*/
-	public IdPlayer getId() {
-		return id;
+    /**
+    * add a new card to the Player's hand
+    */
+    public addCard(Card _card) {
+      myMinions.add(_card);
+    }
+  
+    public <Card> getMyCards() {
+      return myCards;
+    }
+
+    /**
+    * add a new minion to the Player's minions
+    */
+    public addMinion(Minion _minion) {
+      myMinions.add(_minion);
+    }
+
+    public <Minion> getMyMinions() {
+      return myMinions;
+    }
+
+  	/**
+  	* Returns value of id
+  	* @return
+  	*/
+  	public IdPlayer getId() {
+  		return id;
+  	}
+
+  	/**
+  	* Sets new value of id
+  	* @param
+  	*/
+  	public void setId(IdPlayer id) {
+  		this.id = id;
+  	}
+
+  	/**
+  	* Returns value of myHero
+  	* @return
+  	*/
+  	public Hero getMyHero() {
+  		return myHero;
+  	}
+
+  	/**
+  	* Sets new value of myHero
+  	* @param
+  	*/
+  	public void setMyHero(Hero myHero) {
+  		this.myHero = myHero;
+  	}
+
+  	/**
+  	* Returns value of myMana
+  	* @return
+  	*/
+  	public int getMyMana() {
+  		return myMana;
+  	}
+
+  	/**
+  	* Sets new value of myMana
+  	* @param
+  	*/
+  	public void setMyMana(int myMana) {
+  		this.myMana = myMana;
+  	}
+
+  	/**
+  	* Default empty Player constructor
+  	*/
+  	public Player() {
+  		super();
+  	}
+
+  	/**
+  	* Default Player constructor
+  	*/
+  	public Player(IdPlayer id, Hero myHero) {
+  		super();
+  		this.id = id;
+  		this.myHero = myHero;
+  		this.myMana = 0;
 	}
-
-	/**
-	* Sets new value of id
-	* @param
-	*/
-	public void setId(IdPlayer id) {
-		this.id = id;
-	}
-
-	/**
-	* Returns value of myHero
-	* @return
-	*/
-	public Hero getMyHero() {
-		return myHero;
-	}
-
-	/**
-	* Sets new value of myHero
-	* @param
-	*/
-	public void setMyHero(Hero myHero) {
-		this.myHero = myHero;
-	}
-
-	/**
-	* Returns value of myMana
-	* @return
-	*/
-	public int getMyMana() {
-		return myMana;
-	}
-
-	/**
-	* Sets new value of myMana
-	* @param
-	*/
-	public void setMyMana(int myMana) {
-		this.myMana = myMana;
-	}
-
-	/**
-	* Default empty Player constructor
-	*/
-	public Player() {
-		super();
-	}
-
-	/**
-	* Default Player constructor
-	*/
-	public Player(IdPlayer id, Hero myHero, int myMana) {
-		super();
-		this.id = id;
-		this.myHero = myHero;
-		this.myMana = myMana;
-	}
-
-  /**
-  * add a new minion to the Player's hand
-  */
-  public addMinionToHand(Minion mewMinion) {
-    myMinions.add(newMinion);
-  }
-
 }
---
+++ MHServer/src/main/java/impl/Tourbillon.java
@@ -0,0 +1,30 @@
+package impl;
+
+import inter.Target;
+import abstracts.Minion;
+import abstracts.Spell;
+import java.util.ArrayList;
+
+public class Tourbillon implements CardAction {
+
+    private Spell mySpell;
+
+    public Tourbillon(Spell _mySpell) {
+        mySpell = _mySpell;
+    }
+
+    public void effect(Target myTarget) {
+/*
+      myTargets = new ArrayList<Minion>;
+      alliedMinions = new ArrayList<Minion>;
+      opponentsMinions = new ArrayList<Minion>;
+
+      alliedMinions = mySpell.getPlayer().getMyMinions();
+      opponentsMinions = mySpell.getPlayer().getOpponentsMinions;
+      myTargets = alliedMinions.addAll(0, opponentsMinions);
+
+      for (Minion object: list) {
+        System.out.println(object);
+      }*/
+    }
+}

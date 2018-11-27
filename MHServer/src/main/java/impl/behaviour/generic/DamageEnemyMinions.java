4877821eefd6dc7fbe2023e8bc00b3499e334aed
GlennPlou <glenn.plouhinec@etu.univ-nantes.fr>
2018-11-27 20:15:35 +0100

    Arcane Explosion behaviour

---
+++ MHServer/src/main/java/impl/behaviour/generic/DamageEnemyMinions.java
@@ -0,0 +1,35 @@
+package impl.behaviour.generic;
+
+import abstracts.Minion;
+import abstracts.Spell;
+import impl.Player;
+import inter.CardAction;
+import inter.Target;
+
+import java.util.ArrayList;
+
+/**
+ * Class representing the special action "DamageEnemyMinions" used for the "Arcane Explosion" spell.
+ * Deal 1 damage to all enemy minions.
+ * @author Raphaël Pagé & Henri Bouvet & Alexandre Melo & Glenn Plouhinec
+ * @version 0.1
+ */
+public class DamageEnemyMinions implements CardAction {
+
+    private Spell mySpell;
+
+    public DamageEnemyMinions(Spell newSpell) {
+        this.mySpell = newSpell;
+    }
+
+    @Override
+    public void effect(Target myTarget) {
+        Player myOpponent = mySpell.getPlayer().getOpponent();
+        ArrayList<Minion> hisMinions = myOpponent.getMyMinions();
+        int damages = mySpell.getDamagePoints();
+
+        for (Minion target : hisMinions) {
+            target.takeDamage(damages);
+        }
+    }
+}

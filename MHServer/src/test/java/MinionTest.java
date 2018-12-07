fdccc53135376ca58e3d717fd94a00929b788f3a
GlennPlou <glenn.plouhinec@etu.univ-nantes.fr>
2018-12-07 12:17:55 +0100

    Début de la classe de test MinionTest

---
+++ MHServer/src/test/java/MinionTest.java
@@ -0,0 +1,31 @@
+import abstracts.Minion;
+import impl.ConcreteMinion;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static abstracts.CardType.COMMON;
+import static abstracts.CardType.MAGE;
+import static abstracts.CardType.PALADIN;
+import static abstracts.CardType.WARRIOR;
+import static org.junit.Assert.assertEquals;
+
+public class MinionTest {
+
+    private Minion sanglier,soldat,chevaucheur,chef,yetiRenoi;
+
+    @BeforeEach
+    void setup() {
+        Map<String, String> map;
+
+        map = new HashMap<>();
+        map.put("taunt", "");
+        soldat = new ConcreteMinion(COMMON,1,1,2,map, null, "soldat du comté de l'or");
+    }
+    @Test
+    void test() {
+
+    }
+}

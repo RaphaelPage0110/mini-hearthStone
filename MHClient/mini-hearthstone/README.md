15ebeae40b0b8528c0aaba934f3d2fefe30f4c4a
RaphaelPage0110 <page.raf@gmail.com>
2018-12-26 12:38:22 +0100

    Bon ca c'est une version propre normalement. Déso

--- MHClient/mini-hearthstone/README.md
+++ MHClient/mini-hearthstone/README.md
@@ -1,4 +1,4 @@
-# MiniHearthstone
+# MiniHearthStone
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.1.4.
 
--- MHClient/mini-hearthstone/src/app/app.component.ts
+++ MHClient/mini-hearthstone/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import * as Stomp from 'stompjs';
-import * as SockJS from 'sockjs-client';
-import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -9,31 +6,5 @@ import $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private serverUrl = 'http://localhost:8080/socket'
-  private title = 'WebSockets chat';
-  private stompClient;
-
-  constructor(){
-    this.initializeWebSocketConnection();
-  }
-
-  initializeWebSocketConnection(){
-    let ws = new SockJS(this.serverUrl);
-    this.stompClient = Stomp.over(ws);
-    let that = this;
-    this.stompClient.connect({}, function(frame) {
-      that.stompClient.subscribe("/chat", (message) => {
-        if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
-          console.log(message.body);
-        }
-      });
-    });
-  }
-
-  sendMessage(message){
-    this.stompClient.send("/app/send/message" , {}, message);
-    $('#input').val('');
-  }
-
+  title = 'mini-hearthStone';
 }

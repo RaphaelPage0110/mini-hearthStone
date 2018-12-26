b3b20d2bc9f91bc6a8cd6a64ca4db77a81d21ed8
RaphaelPage0110 <page.raf@gmail.com>
2018-12-26 12:00:09 +0100

    Setup the angular client

--- MHClient/mini-hearthstone/src/app/app.component.ts
+++ MHClient/mini-hearthstone/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mini-hearthstone';
+  private serverUrl = 'http://localhost:8080/socket'
+  private title = 'WebSockets chat';
+  private stompClient;
+
+  constructor(){
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection(){
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe("/chat", (message) => {
+        if(message.body) {
+          $(".chat").append("<div class='message'>"+message.body+"</div>")
+          console.log(message.body);
+        }
+      });
+    });
+  }
+
+  sendMessage(message){
+    this.stompClient.send("/app/send/message" , {}, message);
+    $('#input').val('');
+  }
+
 }

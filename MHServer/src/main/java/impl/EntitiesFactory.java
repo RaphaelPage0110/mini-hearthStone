5383c35192a58858a1b116db42baf9a9e422c8e5
GlennPlou <glenn.plouhinec@etu.univ-nantes.fr>
2019-01-09 23:23:23 +0100

    ConcreteMinion Factory

--- MHServer/src/main/java/impl/EntitiesFactory.java
+++ MHServer/src/main/java/impl/EntitiesFactory.java
@@ -31,4 +31,56 @@ public class EntitiesFactory {
 
         return concreteHero;
     }
+
+    public ConcreteMinion createMinion(String minionName) {
+
+        ConcreteMinion concreteMinion = new ConcreteMinion();
+        Map<String, String> abilityKeyword = new HashMap<>();
+        Map<String, String> deathRattle = new HashMap<>();
+
+        switch(minionName) {
+            case "Chef de raid":
+                abilityKeyword.put("buffAlliedMinions", "1");
+                deathRattle.put("removeAura", "");
+                concreteMinion = new ConcreteMinion(CardType.COMMON, 3, 2, 2, abilityKeyword, deathRattle, minionName, "https://bit.ly/2SA51zw", "+1 d'attaque pour tous les serviteurs alliés du plateau");
+                break;
+            case "Champion frisselame":
+                abilityKeyword.put("charge", "");
+                abilityKeyword.put("lifeSteal", "");
+                concreteMinion = new ConcreteMinion(CardType.PALADIN, 4, 3, 2, abilityKeyword, deathRattle, minionName, "https://bit.ly/2C0mH0d", "Charge, Vol de vie");
+                break;
+            case "Sanglier Brocheroc":
+                concreteMinion = new ConcreteMinion(CardType.COMMON, 1, 1, 1, abilityKeyword, deathRattle, minionName, "https://bit.ly/2R8Y4Zy", "");
+                break;
+            case "Soldat du comté de l'or":
+                abilityKeyword.put("taunt", "");
+                concreteMinion = new ConcreteMinion(CardType.COMMON, 1, 1, 2, abilityKeyword, deathRattle, minionName, "https://bit.ly/2shdhch", "Provocation");
+                break;
+            case "Yéti noroit":
+                concreteMinion = new ConcreteMinion(CardType.COMMON, 4, 4, 5, abilityKeyword, deathRattle, minionName, "https://bit.ly/2LO6Sy3", "Provocation");
+                break;
+            case "Chevaucheur de loup":
+                abilityKeyword.put("charge", "");
+                concreteMinion = new ConcreteMinion(CardType.COMMON, 3, 3, 1, abilityKeyword, deathRattle, minionName, "https://bit.ly/2F4e5td", "Charge");
+                break;
+            case "Image miroir":
+                abilityKeyword.put("taunt", "");
+                concreteMinion = new ConcreteMinion(CardType.SPECIAL, 1, 0, 2, abilityKeyword, deathRattle, minionName, "https://bit.ly/2Sz8Ftr", "Provocation");
+                break;
+            case "Mouton":
+                concreteMinion = new ConcreteMinion(CardType.SPECIAL, 1, 1, 1, abilityKeyword, deathRattle, minionName, "https://bit.ly/2SBLqiu", "");
+                break;
+            case "Recrue de la main d'argent":
+                concreteMinion = new ConcreteMinion(CardType.SPECIAL, 1, 1, 1, abilityKeyword, deathRattle, minionName, "https://bit.ly/2C01Ny3", "");
+                break;
+            case "Avocat commis d'office":
+                abilityKeyword.put("taunt", "");
+                concreteMinion = new ConcreteMinion(CardType.WARRIOR, 2, 0, 7, abilityKeyword, deathRattle, minionName, "https://bit.ly/2TpGxsu", "Provocation");
+                break;
+            default:
+                break;
+        }
+
+        return concreteMinion;
+    }
 }

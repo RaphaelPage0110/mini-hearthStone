df1c535fe85c6a94c86642bcdc7996e15fa4948c
Raphael Page <page.raf@gmail.com>
2019-01-13 16:32:39 +0100

    Ménage

---
+++ MHClient/mini-hearthstone/src/app/mana/mana.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-mana',
+    templateUrl: './mana.component.html',
+    styleUrls: ['./mana.component.css']
+})
+export class ManaComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
